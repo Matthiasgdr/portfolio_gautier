653367f9ed42871e50d5ced077034db3e95c6a61
Gautier-Burias <gautier.burias92@gmail.com>
2019-06-09 22:02:39 +0200

    :sparkles: Ux-UI and Art page finished

---
+++ bundler/webpack.common.js
@@ -0,0 +1,84 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+    devtool: 'sourcemap',
+    plugins:
+    [
+        new CopyWebpackPlugin([ { from: 'static' } ]),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: path.resolve(__dirname, '../src/index.html')
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'code',
+            template: path.resolve(__dirname, '../src/code.html')
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'ux-ui',
+            template: path.resolve(__dirname, '../src/ux-ui.html')
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'art',
+            template: path.resolve(__dirname, '../src/art.html')
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'motion',
+            template: path.resolve(__dirname, '../src/motion.html')
+        }),
+    ],
+    entry: './src/index.js',
+    output:
+    {
+        filename: 'bundle.[hash].js',
+        path: path.resolve(__dirname, '../dist')
+    },
+    module:
+    {
+        rules:
+        [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use:
+                [
+                    'babel-loader'
+                ]
+            },
+            {
+                test: /\.(jpg|png|gif|svg)$/,
+                use:
+                [
+                    {
+                        loader: 'file-loader',
+                        options:
+                        {
+                            outputPath: 'images/'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(ttf|woff|woff2|eot|otf)$/,
+                use:
+                [
+                    {
+                        loader: 'file-loader',
+                        options:
+                        {
+                            outputPath: 'fonts/'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.html$/,
+                use:
+                [
+                    'html-loader'
+                ]
+            }
+        ]
+    }
+}
---
+++ readme.md
@@ -0,0 +1,26 @@
+# Gautier Burias Portfolio
+
+This is my portfolio that I made with https://github.com/Matthiasgdr.
+
+## Installation
+
+It's very simple, just open your terminal and write thoses 2 commands :
+
+
+```bash
+npm i
+```
+
+```bash
+npm run dev
+```
+
+The site will open up on your device
+
+## What's the point of this Portfolio ?
+
+This site is using Html/Css/Js with Webpack for now.
+
+It help me showing my work to the world! 
+Soon, I'm planning to change alot of things and improving it by adding more animations and more projects. 
+Using Vue.js or React.js to improve the portfolio code would be also very interesting.
